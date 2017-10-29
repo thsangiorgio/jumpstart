2953dad7e56a1f28f07cb5431d650d9f81da1300
thsangiorgio <th.sangiorgio@gmail.com>
2017-10-29 17:20:55 +0000

    carousel update

---
+++ resources/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.carousel').slick({
+    // dots: false;
+    autoplaySpeed: 4000,
+    arrows: false,
+    autoplay: true,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    draggable: false
+  });
+});
