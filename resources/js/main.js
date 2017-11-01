ae67520970800711aabfa1686885525addb7faca
thsangiorgio <th.sangiorgio@gmail.com>
2017-11-01 07:36:41 +0000

    final commit

--- resources/js/main.js
+++ resources/js/main.js
@@ -7,4 +7,17 @@ $(document).ready(function() {
     pauseOnFocus: false,
     pauseOnHover: false
   });
+
+  var scrollDistance = 0;
+  $(window).scroll(function() {
+    var scrollTop = $(this).scrollTop();
+    if (scrollTop - scrollDistance > 50) {
+      var navBarHeight = $('.navbar').css('height');
+      $('.navbar').animate({top: '-' + navBarHeight}, 150);
+      scrollDistance = scrollTop;
+    } else if (scrollDistance - scrollTop > 50) {
+      $('.navbar').animate({top: '0px'}, 150);
+      scrollDistance = scrollTop;
+    }
+  });
 });
